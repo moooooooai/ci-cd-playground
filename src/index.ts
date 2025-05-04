6b35a1f5daf3dc85b7a924265754d085aa67dedc
moai <moai@moaiui-noteubug.local>
2025-05-04 16:42:39 +0900

    feat

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번에서요 대기중");
+});
