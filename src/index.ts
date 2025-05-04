eb501fbb5c22aba5ffc9ecd219eb6a5e854d9b18
moai <moai@moaiui-noteubug.local>
2025-05-04 17:20:53 +0900

    lint

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const 개발할대필요한줄알았는데사실은필요가없는변수 = 3;
-
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번에서요 대기중");
 });
